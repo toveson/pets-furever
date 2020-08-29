7a049a43ecbba422ecdc8785b4e912978fba6dd6
Trever O <toveson9522@gmail.com>
2020-08-29 10:14:46 -0600

    added html, css anf js files

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.carousel').carousel();
+    console.log('hi this is running');
+  });
+
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true
+  });
+        
