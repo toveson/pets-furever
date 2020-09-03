ec361d752867fc778def5e7849f679da1a5ef97b
dianalynshull <dianalynshull@gmail.com>
2020-09-02 18:20:38 -0600

    created function to prevent multiple checkboxes from being selected. Also added no preference options to size, age, and distance selections

--- assets/script.js
+++ assets/script.js
@@ -12,121 +12,116 @@ const ageSelect = $(".age-select");
 const distanceSelect = $(".distance-select");
 // image variables
 let petImg;
-
-$(".uk-checkbox").on("click", function() {
-  let checkboxSelect = ($(this).parent()).parent()[0];
-  
-  if (checkboxSelect === houseTrainedSelect) {
-    console.log("house trained")
-  }
-  else if (checkboxSelect === catsOkSelect) {
-    console.log("cats okay")
-  }
-  else if (checkboxSelect === dogsOkSelect) {
-    console.log("dogs okay")
-  }
-  else if (checkboxSelect === kidsOkSelect) {
-    console.log("kids okay")
-  }
-  else if (checkboxSelect === genderSelect) {
-    console.log("gender")
-  }
-})
-
 populatePage();
-
+// populates the page with dynamic data from API
 function populatePage() {
-  atRiskPets();
-  breedPop();
+	atRiskPets();
+	breedPop();
 }
-
 // Creates images for at risk dogs to add to front page on load
 function atRiskPets() {
-  $.ajax({
-    url: rgUrl + "search/available/dogs",
-    method: "GET",
-    headers: {
-      "Content-Type": "application/vnd.api+json",
-      "Authorization": rgKey,
-    },
-  }).then(function(response) {
-    console.dir(response);
-    for (let i = 0; i < response.data.length; i++) {
-      petImg = response.data[i].attributes.pictureThumbnailUrl;
-      imgLi = $("<li></li>");
-      imgLi.addClass("uk-width-1-6");
-      petImgEl = $("<img>");
-      petImgEl.attr("src", petImg);
-
-      $(".at-risk").append(imgLi);
-      imgLi.append(petImgEl);
-    }
-  });
+	$.ajax({
+		url: rgUrl + "search/available/dogs",
+		method: "GET",
+		headers: {
+			"Content-Type": "application/vnd.api+json",
+			"Authorization": rgKey,
+		},
+	}).then(function(response, imgLi, petImgEl) {
+		for(let i = 0; i < response.data.length; i++) {
+			petImg = response.data[i].attributes.pictureThumbnailUrl;
+			imgLi = $("<li></li>");
+			imgLi.addClass("uk-width-1-6");
+			petImgEl = $("<img>");
+			petImgEl.attr("src", petImg);
+			$(".at-risk").append(imgLi);
+			imgLi.append(petImgEl);
+		}
+	});
 }
 // populates breed selection options
 function breedPop() {
-  $.ajax({
-    url: rgUrl + "species/8/breeds/?limit=300",
-    method: "GET",
-    headers: {
-      "Content-Type": "application/vnd.api+json",
-      "Authorization": rgKey,
-    },
-  }).then(function(response) {
-    console.dir(response);
-    for (let i = 0; i < response.data.length; i++) {
-      breedOp = response.data[i].attributes.name;
-      breedOpEl = $("<option></option>");
-      breedOpEl.text(breedOp);
-      $(".breed-select").append(breedOpEl);
-    }
-  });
+	$.ajax({
+		url: rgUrl + "species/8/breeds/?limit=300",
+		method: "GET",
+		headers: {
+			"Content-Type": "application/vnd.api+json",
+			"Authorization": rgKey,
+		},
+	}).then(function(response, breedOp, breedOpEl) {
+		for(let i = 0; i < response.data.length; i++) {
+			breedOp = response.data[i].attributes.name;
+			breedOpEl = $("<option></option>");
+			breedOpEl.text(breedOp);
+			$(".breed-select").append(breedOpEl);
+		}
+	});
 }
-
+// Unchecks boxes in an option group so that only the user option is shown
+function onlyCheckUserSelect(checkedGroup) {
+	for(let i = 0; i < (checkedGroup.children).length; i++) {
+		let checkbox = (checkedGroup).children[i].children[0];
+		checkbox.checked = false;
+	}
+}
 // Event Listeners
 // Changes text based on size selection
-sizeSelect.on("input", function() {
-  sizeText = $(".size-text")
-    if (parseInt(this.value) === 1) {
-      sizeText.text("Size Group: Small");
-    }
-    else if (parseInt(this.value) === 2) {
-      sizeText.text("Size Group: Medium");
-    }
-    else if (parseInt(this.value) === 3) {
-      sizeText.text("Size Group: Large");
-    }
-    else if (parseInt(this.value) === 4) {
-      sizeText.text("Size Group: Extra Large");
-    }
-})
+sizeSelect.on("input", function(sizeText) {
+	sizeText = $(".size-text");
+	if(parseInt(this.value) === 0) {
+    sizeText.text("Size Group: No Preference");
+  } else if(parseInt(this.value) === 1) {
+		sizeText.text("Size Group: Small");
+	} else if(parseInt(this.value) === 2) {
+		sizeText.text("Size Group: Medium");
+	} else if(parseInt(this.value) === 3) {
+		sizeText.text("Size Group: Large");
+	} else if(parseInt(this.value) === 4) {
+		sizeText.text("Size Group: Extra Large");
+	}
+});
 // Changes text based on age selection
-ageSelect.on("input", function() {
-  ageText = $(".age-text")
-    if (parseInt(this.value) === 1) {
-      ageText.text("Age Group: Puppy");
-    }
-    else if (parseInt(this.value) === 2) {
-      ageText.text("Age Group: Young");
-    }
-    else if (parseInt(this.value) === 3) {
-      ageText.text("Age Group: Adult");
-    }
-    else if (parseInt(this.value) === 4) {
-      ageText.text("Age Group: Senior");
-    }
-})
+ageSelect.on("input", function(ageText) {
+	ageText = $(".age-text");
+	if(parseInt(this.value) === 0) {
+    ageText.text("Age Group: No Preference");
+  } else if(parseInt(this.value) === 1) {
+		ageText.text("Age Group: Puppy");
+	} else if(parseInt(this.value) === 2) {
+		ageText.text("Age Group: Young");
+	} else if(parseInt(this.value) === 3) {
+		ageText.text("Age Group: Adult");
+	} else if(parseInt(this.value) === 4) {
+		ageText.text("Age Group: Senior");
+	}
+});
 // Changes text based on distance option
-distanceSelect.on("input", function() {
-  distanceText = $(".distance-text");
-  distanceValue = parseInt(this.value);
-  distanceText.text("Distance: " + distanceValue + " Miles");
-})
-
-
+distanceSelect.on("input", function(distanceText, distanceValue) {
+	distanceText = $(".distance-text");
+	distanceValue = parseInt(this.value);
+	distanceText.text("Distance: " + distanceValue + " Miles");
+});
+// only selects user's input for checkbox options
+$(".uk-checkbox").on("click", function() {
+	let checkedGroup = ($(this).parent()).parent()[0];
+	if(checkedGroup === houseTrainedSelect) {
+		onlyCheckUserSelect(checkedGroup);
+		this.checked = true;
+	} else if(checkedGroup === catsOkSelect) {
+		onlyCheckUserSelect(checkedGroup);
+		this.checked = true;
+	} else if(checkedGroup === dogsOkSelect) {
+		onlyCheckUserSelect(checkedGroup);
+		this.checked = true;
+	} else if(checkedGroup === kidsOkSelect) {
+		onlyCheckUserSelect(checkedGroup);
+		this.checked = true;
+	} else if(checkedGroup === genderSelect) {
+		onlyCheckUserSelect(checkedGroup);
+		this.checked = true;
+	}
+});
 // function to get user selections
-  // use click listeners on each selection and stores in the variables
-
+// use click listeners on each selection and stores in the variables
 //function to fetch API data based on user selections
-
 //function to populate html elements based on reponse
